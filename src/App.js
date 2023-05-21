9fdcdea582d866d494c28a6a423e7b68b95fc2d6
Aditya Sarawat <adityasarawat@gmail.com>
2023-05-21 12:55:32 +0530

    fixes

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, {useState} from "react";
 import {BrowserRouter as Router, Navigate, Route, Routes} from "react-router-dom";
 import Navbar from "./components/navbar";
 import Blocks from "./src/blocks";
@@ -7,24 +7,33 @@ import Console from "./src/console";
 import "./css/background.css";
 import "./index.css";
 
+export const UserContext = React.createContext("");
+
 const App = () => {
-  return (
-    <Router>
-      <div className="paper-background">
-        <Navbar />
-        <div
-          className="mainBody"
-          style={{ marginTop: "55px", height: "calc(100vh - 55px)" }}
-        >
-          <Routes>
-            <Route path="/" element={<Navigate to="/blocks" />} />
-            <Route path="/blocks" element={<Blocks />} />
-            <Route path="/console" element={<Console />} />
-          </Routes>
-        </div>
-      </div>
-    </Router>
-  );
+    const [json, setJson] = useState("");
+    const [javascriptCode, setJavascriptCode] = useState("");
+    const [pythonCode, setPythonCode] = useState("");
+
+    return (
+        <Router>
+            <div className="paper-background">
+                <UserContext.Provider
+                    value={{json, setJson, javascriptCode, setJavascriptCode, pythonCode, setPythonCode}}>
+                    <Navbar/>
+                    <div
+                        className="mainBody"
+                        style={{marginTop: "55px", height: "calc(100vh - 55px)"}}
+                    >
+                        <Routes>
+                            <Route path="/" element={<Navigate to="/blocks"/>}/>
+                            <Route path="/blocks" element={<Blocks/>}/>
+                            <Route path="/console" element={<Console/>}/>
+                        </Routes>
+                    </div>
+                </UserContext.Provider>
+            </div>
+        </Router>
+    );
 };
 
 export default App;
--- src/src/blocks.js
+++ src/src/blocks.js
@@ -1,70 +1,66 @@
-import React, { useState } from "react";
-import { BlocklyWorkspace } from "react-blockly";
-import * as Blockly from "blockly/core";
+import React, {useContext} from "react";
+import {BlocklyWorkspace} from "react-blockly";
 import "blockly/blocks";
-import { javascriptGenerator } from "blockly/javascript";
-import { pythonGenerator } from "blockly/python";
+import {javascriptGenerator} from "blockly/javascript";
+import {pythonGenerator} from "blockly/python";
 import AceEditor from "react-ace";
 import "ace-builds/src-noconflict/mode-javascript";
 import "ace-builds/src-noconflict/mode-python";
 import "ace-builds/src-noconflict/theme-monokai";
-import "ace-builds/src-noconflict/ext-language_tools"; // Import language tools for autocomplete
+import "ace-builds/src-noconflict/ext-language_tools";
 import "../css/blocks.css";
 
 import workspaceConfig from "../config/workspaceConfig";
 import toolboxCategories from "../config/toolboxCategories";
 
+import {UserContext} from '../App';
+
 export default function App() {
-  const [json, setJson] = useState("");
-  const [javascriptCode, setJavascriptCode] = useState("");
-  const [pythonCode, setPythonCode] = useState("");
+    const {json, setJson, setJavascriptCode, pythonCode, setPythonCode} = useContext(UserContext);
 
-  const initialJson = {};
+    function workspaceDidChange(workspace) {
+        const javascriptCode = javascriptGenerator.workspaceToCode(workspace);
+        setJavascriptCode(javascriptCode);
 
-  function workspaceDidChange(workspace) {
-    const javascriptCode = javascriptGenerator.workspaceToCode(workspace);
-    setJavascriptCode(javascriptCode);
+        const pythonCode = pythonGenerator.workspaceToCode(workspace);
+        setPythonCode(pythonCode);
+    }
 
-    const pythonCode = pythonGenerator.workspaceToCode(workspace);
-    setPythonCode(pythonCode);
-
-    console.log(javascriptCode);
-  }
-
-  return (
-    <div className="container">
-      <div className="blockly-container">
-        <BlocklyWorkspace
-          toolboxConfiguration={toolboxCategories}
-          className="blockly-workspace"
-          workspaceConfiguration={workspaceConfig}
-          onWorkspaceChange={workspaceDidChange}
-          onJsonChange={setJson}
-        />
-      </div>
-      <div className="ace-container">
-        <AceEditor
-          mode="python"
-          theme="monokai"
-          value={javascriptCode}
-          onChange={setJavascriptCode}
-          readOnly={true}
-          fontSize={15}
-          showPrintMargin={true}
-          highlightActiveLine={false}
-          setOptions={{
-            showLineNumbers: true,
-            showGutter: true,
-            highlightGutterLine: false,
-            lineHeight: 1.5
-          }}
-          style={{
-            pointerEvents: "none"
-          }}
-          height="100vh"
-          width="100%"
-        />
-      </div>
-    </div>
-  );
+    return (
+        <div className="container">
+            <div className="blockly-container">
+                <BlocklyWorkspace
+                    toolboxConfiguration={toolboxCategories}
+                    className="blockly-workspace"
+                    workspaceConfiguration={workspaceConfig}
+                    onWorkspaceChange={workspaceDidChange}
+                    onJsonChange={setJson}
+                    initialJson={json}
+                />
+            </div>
+            <div className="ace-container">
+                <AceEditor
+                    mode="python"
+                    theme="monokai"
+                    value={pythonCode}
+                    onChange={setPythonCode}
+                    readOnly={true}
+                    fontSize={15}
+                    showPrintMargin={true}
+                    highlightActiveLine={false}
+                    setOptions={{
+                        showLineNumbers: true,
+                        showGutter: true,
+                        highlightGutterLine: false,
+                        lineHeight: 1.5
+                    }}
+                    style={{
+                        pointerEvents: "none"
+                    }}
+                    height="100vh"
+                    width="100%"
+                />
+            </div>
+        </div>
+    );
 }
