7e42a0dd6e327390273a8f9ab4ba45e48c99004f
unknown <sarthakarya39@gmail.com>
2023-06-05 15:50:40 +0530

    added dynamic port api

--- server/pfirmata.py
+++ server/pfirmata.py
@@ -2,7 +2,7 @@ from pyfirmata import Arduino, util
 import time
 
 # specify the port to which the arduino is connected
-board = Arduino('COM6')
+board = Arduino('COM7')
 
 # get a reference to the servo pin
 servo_pin = board.get_pin('d:9:s')
